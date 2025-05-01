022bb78a811124259acc9571bf4c64ee5791eeb9
frankbria <frank.bria@gmail.com>
2025-04-30 22:52:54 -0700

    fallbackRedirectUrl change

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -13,7 +13,7 @@ export default function SignInPage() {
           routing="path"
           path="/sign-in"
           signUpUrl="/sign-up"
-          afterSignInUrl="/admin"
+          fallbackRedirectUrl="/admin"
         />
       </div>
     </div>
